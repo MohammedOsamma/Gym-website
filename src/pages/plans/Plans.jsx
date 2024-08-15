32bbaa14e954a4c98fb4e6b068b13c0657462199
MohammedOsamma <mo856915i@gmail.com>
2024-08-15 21:55:23 +0200

    Finish Plans Page

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,6 +1,37 @@
+import Header from "./../../component/Header";
+import Card from "./../../UI/Card";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import { plans } from "../../data";
 import "./plans.css";
+
 const Plans = () => {
-  return <div>Plans</div>;
+  return (
+    <>
+      <Header title="Membership Plans" image={HeaderImage}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ipsum
+        illum quae doloribus nostrum debitis.
+      </Header>
+      <section className="plans">
+        <div className="container plans__container">
+          {plans.map(({ id, name, desc, price, features }) => (
+            <Card className="plan" key={id}>
+              <h3>{name}</h3>
+              <small>{desc}</small>
+              <h1>{price}</h1>
+              <h2>/mo</h2>
+              <h4>Features</h4>
+              {features.map(({ feature, available }, index) => (
+                <p key={index} className={!available ? "disabled" : ""}>
+                  {feature}
+                </p>
+              ))}
+              <button className="btn lg ">Choose Plan</button>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Plans;
