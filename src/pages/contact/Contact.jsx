685e0355a8e7c2e277df173c2fa19e485a738a9c
MohammedOsamma <mo856915i@gmail.com>
2024-08-15 16:19:06 +0200

    Finish Contact , Gallery  and NotFound pages

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,33 @@
+import HeaderImage from "../../images/header_bg_2.jpg";
+import Header from "./../../component/Header";
+import { MdEmail } from "react-icons/md";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 import "./contact.css";
-
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Header title="Get in Touch" image={HeaderImage}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+        assumenda magnam perferendis earum, repudiandae consectetur.
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a href="" target="_blank" rel="noreferrer noopener">
+              <MdEmail />
+            </a>
+            <a href="" target="_blank" rel="noreferrer noopener">
+              <BsMessenger />
+            </a>
+            <a href="" target="_blank" rel="noreferrer noopener">
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,31 @@
 import "./gallery.css";
+import HeaderImage from "../../images/header_bg_3.jpg";
+import Header from "./../../component/Header";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const galleryLength = 15;
+  const images = [];
+
+  for (var i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+  return (
+    <>
+      <Header title="Our Gallery" image={HeaderImage}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In ullam
+        reiciendis quas fugiat nulla. Unde!
+      </Header>
+      <section className="gallery">
+        <div className="container gallery__container">
+          {images.map((image, index) => (
+            <article key={index}>
+              <img src={image} alt={`GalleryImage ${index + 1}`} />
+            </article>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Gallery;
